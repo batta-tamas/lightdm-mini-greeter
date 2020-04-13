900dbe398e34d068ed95556e2d3fc2691e4d82a6
bat <batta.tamas@t-online.hu>
2020-04-13 16:45:44 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -34,6 +34,8 @@ typedef struct Config_ {
     GdkRGBA  *border_color;
     gchar    *border_width;
     guint     layout_spacing;
+    gint      x_offset;
+    gint      y_offset;
     // Password Input
     GdkRGBA  *password_color;
     GdkRGBA  *password_background_color;
